2e7fd3041e72ae60983d31b8ff0e8dd2060dfbe0
Angas <angasreid@gmail.com>
2014-10-20 16:57:42 -0400

    First commit

---
+++ app/controllers/ditties_controller.rb
@@ -0,0 +1,62 @@
+class DittiesController < ApplicationController
+    before_action :set_ditty, only: [:edit, :update, :destroy]
+  def index
+    @ditties = Ditty.all
+  end
+
+  def show
+    @ditty = Ditty.find(params[:id])
+    @comment = Comment.new
+    # @comment = Comment.where(params[:id])
+  end
+
+  def new
+    @ditty = Ditty.new
+  end
+
+  def create
+    @ditty = Ditty.new(ditties_params)
+    if @ditty.save
+      flash[:notice] = "New ditty posted"
+      redirect_to ditties_path
+    else
+      flash[:notice] = "Something went wrong."
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @ditty.update(ditties_params)
+      flash[:notice] = "Ditty updated"
+      redirect_to @ditty
+    else
+      flash[:alert] = "Something went wrong"
+      render :edit
+    end
+  end
+
+  def destroy
+    @ditty.destroy
+    flash[:notice] = "Ditty deleted"
+    redirect_to users_path
+  end
+
+  private
+
+  def ditties_params
+      params.require(:ditty).permit(:body, :player)
+  end
+
+
+  def set_ditty
+    @ditty = Ditty.find(params[:id])
+  end
+
+
+end
+
+
+
